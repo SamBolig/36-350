52132b8ab2720a31351ce81cf08d151e97eea319
Sam <sbolig@andrew.cmu.edu>
2018-04-19 13:52:29 -0400

    Got 2b) to work! Needed better indexing of ps and had to generate more data in the 'covarites' function in 2a)

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -8,18 +8,27 @@ generate_data = function(n, p){
 
 model_select = function(covariates, responses, cutoff){
   coeffs = summary(lm(responses ~ covariates))$coefficients
-  sig.covs = which(coeffs[(ncol(covariates+1)*3 + 1):(ncol(covariates+1)*4)] <= cutoff)
-  new.coeffs = summary(lm(responses ~ covariates[,sig.covs]))$coefficients
+  p = coeffs[((ncol(covariates)+1)*3 + 1):((ncol(covariates)+1)*4)]
+  sig.vars = covariates[,which(p <= cutoff) - 1]
+  new.coeffs = summary(lm(responses ~ sig.vars))$coefficients
   p.vector = c()
-  p.vector = cbind(p.vector, new.coeffs[,4])
+  p.vector = cbind(p.vector, new.coeffs[((ncol(sig.vars)+1)*3 + 1):((ncol(sig.vars)+1)*4)])
   return(
     list(p.vector)
   )
 }
 
-data = generate_data(10,3)
+data = generate_data(100,9)
 covariates = data[[1]]
 responses = data[[2]]
-summary(lm(responses ~ covariates))$coefficients[((ncol(covariates)+1)*3 + 1):((ncol(covariates)+1)*4)]
+ps = summary(lm(responses ~ covariates))$coefficients[((ncol(covariates)+1)*3 + 1):((ncol(covariates)+1)*4)]
+
+which(ps <= 0.5) - 1
+
+sig.vars = covariates[,c((which(ps <= 0.5) - 1))]
+
+new.coeffs = summary(lm(responses ~ sig.vars))$coefficients
+p.vector = c()
+p.vector = cbind(p.vector, new.coeffs[((ncol(sig.vars)+1)*3 + 1):((ncol(sig.vars)+1)*4)])
 
 model_select(generate_data(5,5)[[1]], generate_data(5,5)[[2]], cutoff = 0.25)
