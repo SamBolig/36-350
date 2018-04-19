95370af5d2da8cbbb00fdc80b727ccd84505b233
Sam <sbolig@andrew.cmu.edu>
2018-04-19 12:55:06 -0400

    2b) filled function first attempt

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -8,5 +8,11 @@ generate_data = function(n, p){
 
 model_select = function(covariates, responses, cutoff){
   coeffs = summary(lm(responses ~ covariates))$coefficients
-  which(coeffs[,4] <= cutoff)
+  sig.covs = which(coeffs[,4] <= cutoff)
+  new.coeffs = summary(lm(responses ~ covariates[,sig.covs]))$coefficients
+  p.vector = c()
+  p.vector = cbind(p.vector, new.coeffs[,4])
+  return(
+    list(p.vector)
+  )
 }
