81d642a1ac7ef4cd0a5143468d908ccaf9d7a33c
Sam <sbolig@andrew.cmu.edu>
2018-04-19 13:39:53 -0400

    exploring problems

--- lab_12_simulation.R
+++ lab_12_simulation.R
@@ -1,5 +1,5 @@
 generate_data = function(n, p){
-  covariates = matrix(rnorm(p), nrow=n, ncol=p)
+  covariates = matrix(rnorm(n*p), nrow=n, ncol=p)
   responses = as.vector(rnorm(n))
   return(
     list(covariates, responses)
@@ -8,7 +8,7 @@ generate_data = function(n, p){
 
 model_select = function(covariates, responses, cutoff){
   coeffs = summary(lm(responses ~ covariates))$coefficients
-  sig.covs = which(coeffs[,4] <= cutoff)
+  sig.covs = which(coeffs[(ncol(covariates+1)*3 + 1):(ncol(covariates+1)*4)] <= cutoff)
   new.coeffs = summary(lm(responses ~ covariates[,sig.covs]))$coefficients
   p.vector = c()
   p.vector = cbind(p.vector, new.coeffs[,4])
@@ -16,3 +16,10 @@ model_select = function(covariates, responses, cutoff){
     list(p.vector)
   )
 }
+
+data = generate_data(10,3)
+covariates = data[[1]]
+responses = data[[2]]
+summary(lm(responses ~ covariates))$coefficients[((ncol(covariates)+1)*3 + 1):((ncol(covariates)+1)*4)]
+
+model_select(generate_data(5,5)[[1]], generate_data(5,5)[[2]], cutoff = 0.25)
